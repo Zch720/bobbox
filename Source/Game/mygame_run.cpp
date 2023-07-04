284270b3c2bafc9cf1d7593a1d00c81d86a4e520
ZhongCH <92720howard@gmail.com>
2023-07-04 11:45:46 +0800

    Remove unuse comments. Make coding style correct.

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -9,62 +9,41 @@
 
 using namespace game_framework;
 
-/////////////////////////////////////////////////////////////////////////////
-// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
-/////////////////////////////////////////////////////////////////////////////
-
-CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
-{
+CGameStateRun::CGameStateRun(CGame* g) : CGameState(g) {
 }
 
-CGameStateRun::~CGameStateRun()
-{
+CGameStateRun::~CGameStateRun() {
 }
 
-void CGameStateRun::OnBeginState()
-{
+void CGameStateRun::OnBeginState() {
 }
 
-void CGameStateRun::OnMove()							// 移動遊戲元素
-{
-	
+void CGameStateRun::OnMove() {
 }
 
-void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
-{
-	
+void CGameStateRun::OnInit() {
 }
 
-void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
-{
-	
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags) {
 }
 
-void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
-{
-	
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags) {
 }
 
-void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
-{
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point) {
 }
 
-void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
-{
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point) {
 }
 
-void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
-{
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point) {
 }
 
-void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
-{
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point) {
 }
 
-void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
-{
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point) {
 }
 
-void CGameStateRun::OnShow()
-{
+void CGameStateRun::OnShow() {
 }
