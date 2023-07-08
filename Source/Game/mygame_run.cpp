d89c077a26640adb1a4d73054a40281c431a39da
ZhongCH <92720howard@gmail.com>
2023-07-08 23:40:24 +0800

    [Bug Fixing] Fixed transition no sound in story.

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -101,11 +101,14 @@ void CGameStateRun::storyKeyInput(char nChar) {
 	if (nChar == VK_SPACE) {
 		if (currentLevel == 0) {
 			currentLevel++;
+			AudioPlayer::PlayTransitionSound();
 			GotoGameState(GAME_STATE_RUN);
 		} else {
+			AudioPlayer::PlayTransitionSound();
 			GotoGameState(GAME_STATE_INIT);
 		}
 	} else if (nChar == VK_ESCAPE) {
+		AudioPlayer::PlayTransitionSound();
 		GotoGameState(GAME_STATE_INIT);
 	}
 }
@@ -153,8 +156,10 @@ void  CGameStateRun::storyShow() {
 	if (!storyManager.IsShowing()) {
 		if (currentLevel == 0) {
 			currentLevel++;
+			AudioPlayer::PlayTransitionSound();
 			GotoGameState(GAME_STATE_RUN);
 		} else {
+			AudioPlayer::PlayTransitionSound();
 			GotoGameState(GAME_STATE_INIT);
 		}
 	}
